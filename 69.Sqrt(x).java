79bc658ec767d93621b38a2e2c6c409475f807b0
Jovian Sanjaya Putra <joviansanjayaputra@Jovians-MacBook-Pro.local>
2025-07-24 01:25:39 +0800

    idk

---
+++ 69.Sqrt(x).java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+       if(x<2){
+        return x;
+       }
+
+       int left=0,right=x,ans=0;
+
+       while(left<=right){
+          int mid = left + (right-left)/2;
+          long square = (long) mid*mid;
+          if(square==x){
+            return mid; // meet perfect square
+          }
+          else if(square<x){
+            ans=mid; // does not meet perfect square 
+            left=mid+1;
+          }
+          else{
+            right=mid-1;
+          }
+       }
+       return ans;
+    }
+}
